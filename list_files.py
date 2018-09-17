32194eddcc4c2433f0304754d076bf23f7e1191b
Johan <johan@groenewald.co.za>
2018-09-18 01:24:36 +0200

    Build reasonable list files modules

---
+++ list_files.py
@@ -0,0 +1,17 @@
+import os
+import uos
+from crc16 import crc16
+
+files = [f for f in os.listdir()]
+files.sort()
+for file in files:
+    s = uos.stat(file)
+    try:
+        with open(file, 'rb') as f:
+            h = crc16(f.read())
+        print('{: <25}  {: >6}  {: >4}'.format(file, h, s[6]))
+    except:
+        print('{: <25}   ERROR  {: >4}'.format(file, s[6]))
+
+import sys
+del sys.modules['list_files']
---
+++ list_files_local.py
@@ -0,0 +1,15 @@
+import os
+from crc16 import crc16
+
+files = [f for f in os.listdir()]
+files.sort()
+for file in files:
+    if file in ['.git', '.gitignore', '.idea']:
+        continue
+    s = os.stat(file)
+    try:
+        with open(file, 'rb') as f:
+            h = crc16(f.read())
+        print('{: <25}  {: >6}  {: >4}'.format(file, h, s[6]))
+    except:
+        print('{: <25}   ERROR  {: >4}'.format(file, s[6]))
