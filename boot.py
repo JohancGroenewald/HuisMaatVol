1a67e009e7d7913cc17a731d409ae653bd0add1c
Johan <johan@groenewald.co.za>
2018-09-23 03:08:14 +0200

    Building responsive component with debounced button

--- boot.py
+++ boot.py
@@ -1,7 +1,7 @@
 # This code will be specific to the ESP8266, ESP8285 and ESP32
 # Hopefully I can find a way to identify the type of SOC at boot time.
 # This start-up code is for the sake of responsiveness.
-from responsive import startup
+from responsive import startup, interrupt_handlers
 
 config = {
     'device': 'ESP8266',
@@ -9,7 +9,8 @@ config = {
     'led': {'pin': 13, 'active': 0},
     'relay': {'pin': 12, 'active': 1}
 }
-button, led, relay = startup(config)
+startup(config)
+interrupt_handlers()
 
 # noinspection PyUnresolvedReferences
 from webrepl import start
--- responsive.py
+++ responsive.py
@@ -1,7 +1,13 @@
 from machine import Pin
+from time import ticks_ms, ticks_diff
+
+button, led, relay = None, None, None
+button_start = None
+button_delay = 500
 
 
 def startup(config):
+    global button, led, relay
     try:
         button = Pin(config['button']['pin'], Pin.IN)
         led = Pin(config['led']['pin'], Pin.OUT)
@@ -12,4 +18,23 @@ def startup(config):
     except:
         button, led, relay = None, None, None
 
-    return button, led, relay
+
+def interrupt_handlers():
+    button.irq(button_interrupt_rising, Pin.IRQ_RISING)
+
+
+def button_interrupt_rising(button):
+    print(button, 'button_interrupt_rising')
+    global button_start
+    button_start = ticks_ms()
+    button.irq(button_interrupt_falling, Pin.IRQ_FALLING)
+
+
+def button_interrupt_falling(button):
+    print(button, 'button_interrupt_falling')
+    global button_start
+    if button_start and ticks_diff(ticks_ms(), button_start) >= button_delay:
+        led.value(not led.value())
+        relay.value(not relay.value())
+    button_start = None
+    button.irq(button_interrupt_rising, Pin.IRQ_RISING)
