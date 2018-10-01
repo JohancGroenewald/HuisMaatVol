9f70a060f25c8693bdf819af2b3a7ed30e654ea0
Johan <johan@groenewald.co.za>
2018-10-01 22:57:10 +0200

    Added more device information to device id script.

--- Tools/device_id.py
+++ Tools/device_id.py
@@ -5,6 +5,8 @@ from ubinascii import hexlify
 device_id = hexlify(wifi.config('mac'), ':').decode().upper()
 
 print('device_id: {}'.format(device_id))
+print(wifi.ifconfig())
+print('ssid: {}'.format(wifi.config('essid')))
 
 from sys import modules
 if __name__ in modules:
