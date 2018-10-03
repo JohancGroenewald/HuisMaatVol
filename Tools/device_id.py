317d98feb9d9034eaba445470d6afcb267d50cee
Johan <johan@groenewald.co.za>
2018-10-03 23:05:06 +0200

    Added more device information to device id script.

--- Tools/device_id.py
+++ Tools/device_id.py
@@ -1,3 +1,9 @@
+from os import listdir                                      #
+config_module = [
+    f[:-3] for f in listdir() if f.startswith('config_sonoff_')
+][0]
+print('config   : {}'.format(config_module))
+
 from network import WLAN, STA_IF
 wifi = WLAN(STA_IF)
 # noinspection PyUnresolvedReferences
@@ -5,8 +11,10 @@ from ubinascii import hexlify
 device_id = hexlify(wifi.config('mac'), ':').decode().upper()
 
 print('device_id: {}'.format(device_id))
-print(wifi.ifconfig())
-print('ssid: {}'.format(wifi.config('essid')))
+print('ssid     : {}'.format(wifi.config('essid')))
+print('ip       : {}'.format(wifi.ifconfig()[0]))
+print('gateway  : {}'.format(wifi.ifconfig()[1]))
+print('dns      : {}'.format(wifi.ifconfig()[3]))
 
 from sys import modules
 if __name__ in modules:
